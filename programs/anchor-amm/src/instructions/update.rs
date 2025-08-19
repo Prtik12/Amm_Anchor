ed22c379ba7e872bbf0ec7b7ceefe773d0b7c6aa
babyJesus12 <babyjesus7695@gmail.com>
2025-08-20 02:18:24 +0530

    update.rs completed

---
+++ programs/anchor-amm/src/instructions/update.rs
@@ -0,0 +1,36 @@
+use anchor_lang::prelude::*;
+
+use crate::{state::Config, error::AmmError};
+
+#[derrive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"config", config.key.as_ref()],
+        bump = config.config_bump,
+    )]
+    pub config: Account<'info, Config>,
+
+}
+
+impl<'info> Update<'info> {
+
+    pub fn lock(&mut self) -> Result<()> {
+        require!(self.config.authority = Some(self.user.key()), AmmError::InvalidAuthority);
+
+        self.config.locked = true;
+
+        Ok(())
+    }
+
+    pub fn unlock(&mut self) -> Result<()> {
+        require!(self.config.authority = Some(self.user.key()), AmmError::InvalidAuthority);
+
+        self.config.locked = false;
+        
+        Ok(())
+    }
+}
