a730e66b6d6ceeed033aa623976b332921ba3f3c
babyJesus12 <babyjesus7695@gmail.com>
2025-08-20 00:56:42 +0530

    error.rs

---
+++ programs/anchor-amm/src/error.rs
@@ -0,0 +1,38 @@
+use anchor_lang::prelude::*;
+use constant_product_curve::CurveError;
+
+#[error_code]
+pub enum AmmError {
+    #[msg("This pool is locked")]
+    PoolLocked,
+    #[msg("Invalid ammount")]
+    InvalidAmount,
+    #[msg("Slippage exceeded")]
+    SlippageExceeded,
+    #[msg("Overflow detected")]
+    Overflow,
+    #[msg("Underflow detected")]
+    Underflow,
+    #[msg("Invalid authority")]
+    InvalidAuthority,
+    #[msg("Invalid precision")]
+    InvalidPrecision,
+    #[msg("Insufficient balance")]
+    Insufficientbalance,
+    #[msg("Zero balance")]
+    ZeroBalance,
+}
+
+impl From<CurveError> for AmmError {
+    fn from(error: CurveError) -> AmmError {
+        match error {
+            CurveError::InvalidPrecision      => AmmError::InvalidPrecision,
+            CurveError::Overflow              => AmmError::Overflow,
+            CurveError::Underflow             => AmmError::Underflow,
+            CurveError::InvalidFeeAmount      => AmmError::InvalidFee,
+            CurveError::InsufficientBalance   => AmmError::Insufficientbalance,
+            CurveError::ZeroBalance           => AmmError::ZeroBalance,
+            CurveError::SlippageLimitExceeded => AmmError::SlippageExceded,
+        }
+    }
+}
