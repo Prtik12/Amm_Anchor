bf548eae1f7a8ecf52c0b2b20d1bc99d83f39889
babyJesus12 <babyjesus7695@gmail.com>
2025-08-25 17:26:53 +0530

    Swap impl

--- programs/anchor-amm/src/instructions/swap.rs
+++ programs/anchor-amm/src/instructions/swap.rs
@@ -63,3 +63,92 @@ pub struct Swap<'info> {
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
 }
+
+impl<'info> Swap<'info> {
+    pub fn swap(
+        &mut self,
+        is_x: bool,
+        amount: u64,
+        min: u64,
+    ) -> Result<()> {
+        require!(self.config.locked == false, AMMError::PoolLocked);
+        require!(amount != 0, AMMError::InvalidAmount);
+
+        let mut curve = ConstantProduct::init(
+            self.vault_x.amount,
+            self.vault_y.amount,
+            self.mint_lp.supply,
+            self.config.fee,
+            Some(6)
+        ).map_err(AMMError::from)?;
+
+        let pair = match is_x {
+            true => LiquidityPair::X,
+            false => LiquidityPair::Y
+        };
+
+        let response = curve.swap(pair, amount, min).map_err(AMMError::from)?;
+
+        require!(response.deposit != 0 && response.withdraw != 0, AMMError::InvalidAmount);
+
+        self.deposit_token(is_x, response.deposit)?;
+        self.withdraw_token(!is_x, response.withdraw)?;
+
+        Ok(())
+        
+    }
+
+    pub fn deposit_token(
+        &mut self,
+        is_x: bool,
+        amount: u64
+    ) -> Result<()> {
+        let(from, to) = match is_x {
+            true => (self.user_x.to_account_info(), self.vault_x.to_account_info()),
+            false => (self.user_y.to_account_info(), self.vault_y.to_account_info())
+        };
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from,
+            to,
+            authority: self.user.to_account_info()
+        };
+
+        let ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer(ctx, amount)
+    }
+
+    pub fn withdraw_token(
+        &mut self,
+        is_x: bool,
+        amount: u64
+    ) -> Result<()> {
+        let(from, to) = match is_x {
+            true => (self.vault_x.to_account_info(), self.user_x.to_account_info()),
+            false => (self.vault_y.to_account_info(), self.user_y.to_account_info())
+        };
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from,
+            to,
+            authority: self.config.to_account_info()
+        };
+
+        let seeds = &[
+            &b"config"[..],
+            &self.config.seed.to_le_bytes(),
+            &[self.config.config_bump]
+        ];
+
+        let signer_seeds = &[&seeds[..]];
+
+        let ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer(ctx, amount)
+    }
+}
