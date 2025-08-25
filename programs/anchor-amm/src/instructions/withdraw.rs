3c7a84dd49a991b6d5a5fe2ebac4006259ced8b7
babyJesus12 <babyjesus7695@gmail.com>
2025-08-25 15:30:21 +0530

    withdraw struct

---
+++ programs/anchor-amm/src/instructions/withdraw.rs
@@ -0,0 +1,74 @@
+use anchor_lang::prelude::*;
+use crate::{AmmError, state::Config};
+
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{transfer, Burn, Mint, Token, TokenAccount, Transfer},
+};
+
+use constant_product_curve::ConstantProduct;
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub mint_x: Account<'info, Mint>,
+    pub mint_y: Account<'info, Mint>,
+
+    #[account(
+        seeds = [b"config", config.seed.to_le_bytes().as_ref()],
+        bump = config.config_bump,
+        has_one = mint_x,
+        has_one = mint_y,
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(
+        mut,
+        seeds = [b"lp", config.key.as_ref()],
+        bump = config.lp_bump,
+    )]
+    pub mint_lp: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+    )]
+    pub vault_x: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub vault_y: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = user,
+    )]
+    pub user_x: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = user,
+    )]
+    pub user_y: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_lp,
+        associated_token::authority = user,
+    )]
+    pub user_lp: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+
+}
